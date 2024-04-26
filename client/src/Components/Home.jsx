c93e58f6badceb090bf70851dc34c1866f0e3b52
Chotu Kumar <115991360+Ck07860786@users.noreply.github.com>
2024-04-26 10:53:00 +0530

    Update Home.jsx

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
     const getALlProducts = async () => {
       try {
         const { data } = await axios.get(
-          "https://mern-e-commerce-server-orcin.vercel.app/api/v1/product/get-product"
+          "http://localhost:8080/api/v1/product/get-product"
         );
         setProducts(data.product);
         console.log("products:",data.product);
@@ -31,7 +31,7 @@ function Home() {
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get(
-        "https://mern-e-commerce-server-orcin.vercel.app/api/v1/category/get-category"
+        "http://localhost:8080/api/v1/category/get-category"
       );
       if (data.success) {
         setCategories(data.category);
